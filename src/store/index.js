cf8007cb2fb6b93733f918534189de04792d9c34
sleepycrow <sleepycrow@posteo.net>
2021-10-30 16:36:37 +0200

    Remove a console.log used for debugging from store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import {createStore, createLogger} from 'vuex'
 
 const debug = process.env.NODE_ENV !== 'production'
 
-console.log(debug)
 export default createStore({
 	strict: debug,
 	plugins: debug ? [createLogger()] : []
