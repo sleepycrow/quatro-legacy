574c9737061fcd802a6c1aa420063f1d51a0ff4b
sleepycrow <sleepycrow@posteo.net>
2022-02-01 00:55:57 +0100

    Fix post grouping for reposted parents

--- src/lib/timeline_utils.js
+++ src/lib/timeline_utils.js
@@ -9,22 +9,31 @@ export function groupThreads(timeline, hideShown = true){
 	var found = {}
 	var outputTimeline = []
 
-	// Loop 1: Find replies in the timeline
+	// Loop 1: Find replies to various posts in the timeline, grab the IDs of their parent posts for later
 	for(let i = 0; i < timeline.length; i++){
 		let post = timeline[i]
 
 		if(typeof(post.in_reply_to_id) === "string" && post.in_reply_to_id.length !== "")
 			needed.push(post.in_reply_to_id)
-		
-		if(needed.includes(post.id))
-			found[post.id] = post
 	}
 
-	// Loop 2: Reorder the timeline
+	// Loop 2: Try to find the parent posts collected earlier
 	for(let i = 0; i < timeline.length; i++){
 		let post = timeline[i]
 
-		if(needed.includes(post.id)) continue;
+		if(needed.includes(post.id))
+			found[post.id] = post
+		
+		if(post.reblog && needed.includes(post.reblog.id))
+			found[post.reblog.id] = post
+	}
+
+	// Loop 3: Group posts with their parents
+	for(let i = 0; i < timeline.length; i++){
+		let post = timeline[i]
+
+		if(needed.includes(post.id) || (post.reblog && needed.includes(post.reblog.id)))
+			continue
 
 		/* eslint-disable  no-prototype-builtins */
 		if(typeof(post.in_reply_to_id) === "string" && found.hasOwnProperty(post.in_reply_to_id)){
